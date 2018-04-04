fe15757ff78338de398c2f98631be1d0c625b331
francischoi97 <francischoi97@gmail.com>
2018-04-04 16:18:07 -0400

    Initial commit
    Got lots of work to do

---
+++ functions.js
@@ -0,0 +1,30 @@
+<script>
+
+function shopFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user hovers outside of it
+window.hover = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+</script>
